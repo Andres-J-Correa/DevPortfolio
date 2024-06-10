a401ca4f14f7bd7291acf2afeff6210886ccd9ce
Andres-J-Correa <andresj.correas@gmail.com>
2024-06-10 09:12:34 -0500

    added startup class

--- server/obj/Debug/net8.0/DevPortfolio.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/DevPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f30418a429704682747aa9388a0b06823300eff")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
